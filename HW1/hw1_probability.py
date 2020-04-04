70fb84a01963717002ba4e3b2d19bced20c6d663
CL <chaelin.kim@kaist.ac.kr>
2020-04-04 18:44:18 +0900

    Add bigram plotting code and save csv file
    bigram plotting code 추가
    csv file 저장 기능 추가

--- HW1/hw1_probability.py
+++ HW1/hw1_probability.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import numpy as np
+import matplotlib
 import matplotlib.pyplot as plt
 
 import nltk
@@ -13,10 +14,42 @@ from string import punctuation
 # List of alphabet
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n',
             'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
-# String of punctuation
+# String of punctuation and numbers
 # 0123456789 !"#$%&'()*+,-./:;<=>?@[\]^_`{|}~
 punc = ' ' + punctuation + '\n'
 punc = '0123456789' + punc
+
+# Draw plot function
+def drawPlot(propTable, conditions, title, size_scale=1000):
+    fig, ax = plt.subplots()
+
+    x, y = np.meshgrid(np.arange(27), np.arange(27))
+
+    im = ax.scatter(
+        x=y,
+        y=x,
+        s=propTable.transpose() * size_scale,
+        c=propTable.transpose(),
+        cmap='RdYlGn',
+        marker='s'
+    )
+
+    ax.set(xticks=np.arange(27), yticks=np.arange(27),
+           xticklabels=conditions, yticklabels=conditions)
+    ax.set_xticks(np.arange(27 + 1) - 0.5, minor=True)
+    ax.set_yticks(np.arange(27 + 1) - 0.5, minor=True)
+    ax.set_xlabel('Y')
+    ax.set_ylabel('X')
+    ax.grid(which='minor')
+    ax.set_xlim([-0.5, 26 + 0.5])
+    ax.set_ylim([-0.5, 26 + 0.5])
+    ax.set_ylim(ax.get_ylim()[::-1])
+
+    ax.set_title(title)
+    # im = ax.imshow(bigramPropTable, cmap='Greys')
+    fig.colorbar(im, ax=ax)
+    fig.tight_layout()
+    plt.show()
 
 # Alice's Adventures in Wonderland by Lewis Carroll 1865
 alice_raw = nltk.corpus.gutenberg.raw("carroll-alice.txt")
@@ -31,14 +64,14 @@ print("### Probability distribution over the 27 outcomes for a randomly selected
 # Probability for each alphabet letter
 for letter in alphabet:
     letter_prop = round(alice_raw.count(letter) / len(alice_raw), 5)
-    # print(letter + ": " + str(letter_prop))
-    print(letter + ": " + str(letter_prop) + " / " + str(alice_raw.count(letter)))
+    print(letter + ": " + str(letter_prop))
+    # print(letter + ": " + str(letter_prop) + " / " + str(alice_raw.count(letter)))
 
 # Probability for numbers, whitespace, enter, and punctuation
 countFunc = lambda l1, l2: len(list(filter(lambda c: c in l2, l1)))
 punc_prop = round(countFunc(alice_raw, punc) / len(alice_raw), 5)
-# print('-' + ": " + str(punc_prop))
-print(' ' + ": " + str(punc_prop) + " / " + str(countFunc(alice_raw, punc)))
+print('-' + ": " + str(punc_prop))
+# print(' ' + ": " + str(punc_prop) + " / " + str(countFunc(alice_raw, punc)))
 
 
 print()
@@ -58,7 +91,7 @@ for ele in alice_bigram:
 
 # Compute frequencies for bigrams
 cfd = nltk.ConditionalFreqDist(alice_bigram_list)
-cfd.tabulate()    # frequencies
+# cfd.tabulate()    # Show frequencies
 
 # Compute probability for bigrams (Marginal probability)
 # For ordered pair XY, compute P(X=x, Y=y)
@@ -73,7 +106,61 @@ for x in range(0, 27):
 
 bigram_df = pd.DataFrame(bigramPropTable, index=conditions, columns=conditions)
 print(bigram_df)
+bigram_df.to_csv(r'C:\Users\Leki\Desktop\Dooo\GCT561_ScientificThinking\HW1\bigram_df.csv')
 
+# Show plot
+drawPlot(bigramPropTable, conditions, "Probability distribution of P(X=x, Y=y) for ordered pair XY", 1000)
+# fig, ax = plt.subplots()
+#
+# x, y = np.meshgrid(np.arange(27), np.arange(27))
+#
+# size_scale = 1000
+# im = ax.scatter(
+#     x = y,
+#     y = x,
+#     s = bigramPropTable.flatten() * size_scale,
+#     c = bigramPropTable,
+#     cmap='RdYlGn',
+#     marker='s'
+# )
+#
+# ax.set(xticks=np.arange(27), yticks=np.arange(27),
+#        xticklabels=conditions, yticklabels=conditions)
+# ax.set_xticks(np.arange(27+1)-0.5, minor=True)
+# ax.set_yticks(np.arange(27+1)-0.5, minor=True)
+# ax.set_xlabel('Y')
+# ax.set_ylabel('X')
+# ax.grid(which='minor')
+# ax.set_xlim([-0.5, 26 + 0.5])
+# ax.set_ylim([-0.5, 26 + 0.5])
+# ax.set_ylim(ax.get_ylim()[::-1])
+#
+# ax.set_title("Probability distribution of P(X=x, Y=y) for ordered pair XY")
+# # im = ax.imshow(bigramPropTable, cmap='Greys')
+# fig.colorbar(im, ax=ax)
+# fig.tight_layout()
+# plt.show()
+
+
+# fig, ax = plt.subplots()
+# x, y = np.meshgrid(np.arange(27), np.arange(27))
+# R = bigramPropTable / bigramPropTable.max() / 2
+# circles = [plt.Circle((j, i), radius=r) for r, j, i in zip(R.flat, x.flat, y.flat)]
+# col = matplotlib.collections.PatchCollection(circles, array=bigramPropTable.flatten(), cmap="RdYlGn")
+# ax.add_collection(col)
+#
+# ax.set(xticks=np.arange(27), yticks=np.arange(27),
+#        xticklabels=conditions, yticklabels=conditions)
+# ax.set_xticks(np.arange(27+1)-0.5, minor=True)
+# ax.set_yticks(np.arange(27+1)-0.5, minor=True)
+# ax.set_xlabel('X')
+# ax.set_ylabel('Y')
+# ax.grid(which='minor')
+#
+# ax.set_title("Probability distribution of P(X=x, Y=y) for ordered pair XY")
+# fig.colorbar(col)
+# fig.tight_layout()
+# plt.show()
 
 print()
 ''' 3. Conditional probability distributions over P(y|x) and P(x|y) '''
@@ -91,6 +178,10 @@ for x in range(0, 27):
 cpd_yx_df = pd.DataFrame(cpd_yx_table, index=conditions, columns=conditions)
 print("* P(y|x) table")
 print(cpd_yx_df)
+cpd_yx_df.to_csv(r'C:\Users\Leki\Desktop\Dooo\GCT561_ScientificThinking\HW1\cpd_yx_df.csv')
+
+# Show plot
+drawPlot(cpd_yx_table, conditions, "Conditional probability distribution of P(Y=y|X=x) for ordered pair XY", 50)
 
 
 # 3-2. P(x|y)
@@ -106,7 +197,10 @@ for x in range(0, 27):
 cpd_xy_df = pd.DataFrame(cpd_xy_table, index=conditions, columns=conditions)
 print("* P(x|y) table")
 print(cpd_xy_df)
+cpd_xy_df.to_csv(r'C:\Users\Leki\Desktop\Dooo\GCT561_ScientificThinking\HW1\cpd_xy_df.csv')
 
+# Show plot
+drawPlot(cpd_xy_table, conditions, "Conditional probability distribution of P(X=x|Y=y) for ordered pair XY", 50)
 
 ''' To Do '''
 # 1. Probability distribution over the 27 outcomes for a randomly selected letter
