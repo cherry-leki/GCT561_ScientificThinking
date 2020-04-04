7d14923b722d47653f4b0e2fb3d55ff5e89b2cff
CL <chaelin.kim@kaist.ac.kr>
2020-04-05 00:52:46 +0900

    Ref code overall for submission
    과제 제출을 위한 코드 정리

--- HW1/hw1_probability.py
+++ HW1/hw1_probability.py
@@ -1,23 +1,11 @@
 import pandas as pd
 import numpy as np
-import matplotlib
 import matplotlib.pyplot as plt
 
 import nltk
-# nltk.download("book")
 from nltk import ngrams
 from string import punctuation
 
-# List of gutenberg corpus
-# print(gutenberg.fileids())
-
-# List of alphabet
-alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n',
-            'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
-# String of punctuation and numbers
-# 0123456789 !"#$%&'()*+,-./:;<=>?@[\]^_`{|}~
-punc = ' ' + punctuation + '\n'
-punc = '0123456789' + punc
 
 # Draw plot function
 def drawPlot(propTable, xTickLabel, yTickLabel, title, size_scale, saveName):
@@ -49,59 +37,80 @@ def drawPlot(propTable, xTickLabel, yTickLabel, title, size_scale, saveName):
     ax.set_ylim(ax.get_ylim()[::-1])
 
     ax.set_title(title, loc='center')
-    # im = ax.imshow(bigramPropTable, cmap='Greys')
     fig.colorbar(im, ax=ax)
     fig.tight_layout()
-    plt.savefig(saveName + '.png', dpi=300, bbox_inches='tight')
-    # plt.show()
+    plt.savefig(r".\\result\\" + saveName + '.png', dpi=300, bbox_inches='tight')
+    plt.show()
 
 
+# Count function for probability of punctuation
 countFunc = lambda l1, l2: len(list(filter(lambda c: c in l2, l1)))
 
+
+''' Prepare the data'''
+# nltk.download("book")
+
+# List of gutenberg corpus
+# print(gutenberg.fileids())
 
 # Alice's Adventures in Wonderland by Lewis Carroll 1865
 alice_raw = nltk.corpus.gutenberg.raw("carroll-alice.txt")
 alice_raw = alice_raw.lower()
 print("length of the book 'Alice's Adventures': " + str(len(alice_raw)))       # Count all letters in alice data
 
+# List of alphabet letters
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n',
+            'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+# String of punctuation and numbers
+# 0123456789 !"#$%&'()*+,-./:;<=>?@[\]^_`{|}~
+punc = ' ' + punctuation + '\n'
+punc = '0123456789' + punc
+
+# Conditions (rows and columns for bigram)
+conditions = alphabet.copy()
+conditions.append('-')
+
 
 ''' 1. Probability distribution over the 27 outcomes for a randomly selected letter '''
 print("### Probability distribution over the 27 outcomes for a randomly selected letter ###")
 
-# For letter X in alphabets and punctuations, compute P(X=x)
-# Probability for each alphabet letter
-letterPropTable = np.zeros((27, 1))
+# Compute probability for bigrams (Marginal probability)
+# For the letter X in alphabets and punctuations, compute P(X=x)
+
+# Probabilities for each alphabet letter
+pd_letter_table = np.zeros((27, 1))
 alphabetCount = 0
 for letter in alphabet:
     letterProp = round(alice_raw.count(letter) / len(alice_raw), 5)
-    letterPropTable[alphabetCount] = letterProp
+    pd_letter_table[alphabetCount] = letterProp
     alphabetCount = alphabetCount + 1
-    # print(letter + ": " + str(letterPropTable))
     # print(letter + ": " + str(letterPropTable) + " / " + str(alice_raw.count(letter)))
 
 # Probability for numbers, whitespace, enter, and punctuation
 punc_prop = round(countFunc(alice_raw, punc) / len(alice_raw), 5)
-letterPropTable[alphabetCount] = punc_prop
-# print('-' + ": " + str(punc_prop))
+pd_letter_table[alphabetCount] = punc_prop
 # print(' ' + ": " + str(punc_prop) + " / " + str(countFunc(alice_raw, punc)))
 
-conditions = alphabet
-conditions.append('-')
+# Dataframe of probability distribution P(X=x) for a randomly selected letter
+letters_df = pd.DataFrame(pd_letter_table, index=conditions, columns=['Prob'])
+print(letters_df)       # print the probability distribution
 
-letters_df = pd.DataFrame(letterPropTable, index=conditions, columns=['Prob'])
-print(letters_df)
-letters_df.to_csv(r'C:\Users\Leki\Desktop\Dooo\GCT561_ScientificThinking\HW1\df_letters.csv')
+# Save the probability distribution data
+letters_df.to_csv(r'C:\Users\Leki\Desktop\Dooo\GCT561_ScientificThinking\HW1\result\df_letters.csv')
 
-
-drawPlot(letterPropTable, conditions, ["Prob"],
-         "Probability distribution of P(X=x)\n for randomly selected letter", 110,
+# Draw and save the plot for the probability distribution
+drawPlot(pd_letter_table, conditions, ["Prob"],
+         "Probability distribution P(X=x)\n for randomly selected letter", 110,
          "fig_letters")
 
 
-
 print()
-''' 2. Probability distribution over the 27x27 possible bigrams xy (Marginal probability) '''
-print("### Probability distribution over the 27x27 possible bigrams xy (Marginal probability) ###")
+
+
+''' 2. Probability distribution over the 27x27 possible bigrams for ordered pair xy '''
+print("### Probability distribution over the 27x27 possible bigrams for ordered pair xy ###")
+
+# Compute bigrams of alice data
 alice_bigram = list(ngrams(alice_raw, 2))
 
 # Change all punctuations to ' ' in bigram
@@ -116,27 +125,27 @@ for ele in alice_bigram:
 
 # Compute frequencies for bigrams
 cfd = nltk.ConditionalFreqDist(alice_bigram_list)
-# cfd.tabulate()    # Show frequencies
+# cfd.tabulate()        # Show frequencies
 
-# Compute probability for bigrams (Marginal probability)
+# Compute probability for bigrams
 # For ordered pair XY, compute P(X=x, Y=y)
-conditions = sorted(cfd.conditions())
-conditions = conditions[1:]
-conditions.append('-')
-
-bigramPropTable = np.zeros((27, 27))
+pd_bigram_table = np.zeros((27, 27))
 for x in range(0, 27):
     for y in range(0, 27):
-        bigramPropTable[x][y] = cfd[conditions[x]][conditions[y]] / cfd.N()
+        pd_bigram_table[x][y] = cfd[conditions[x]][conditions[y]] / cfd.N()
 
-bigram_df = pd.DataFrame(bigramPropTable, index=conditions, columns=conditions)
-print(bigram_df)
-bigram_df.to_csv(r'C:\Users\Leki\Desktop\Dooo\GCT561_ScientificThinking\HW1\df_bigram.csv')
+# Dataframe of probability distribution P(X=x, Y=y) for ordered pair XY
+bigram_df = pd.DataFrame(pd_bigram_table, index=conditions, columns=conditions)
+print(bigram_df)       # print the probability distribution
 
-# Show plot
-drawPlot(bigramPropTable, conditions, conditions,
-         "Probability distribution of P(X=x, Y=y) for ordered pair XY", 1000,
+# Save the probability distribution data
+bigram_df.to_csv(r'C:\Users\Leki\Desktop\Dooo\GCT561_ScientificThinking\HW1\result\df_bigram.csv')
+
+# Draw and save the plot for the probability distribution
+drawPlot(pd_bigram_table, conditions, conditions,
+         "Probability distribution P(X=x, Y=y) for ordered pair XY", 1000,
          "fig_bigram")
+
 
 print()
 
@@ -144,25 +153,32 @@ print()
 ''' 3. Conditional probability distributions over P(y|x) and P(x|y) '''
 print("### Conditional probability distributions over P(y|x) and P(x|y) ###")
 #  3-1. P(y|x)
+# Compute conditional probability distributions for ordered pair XY
 # For ordered pair XY, compute P(Y=y|X=x) = P(X=x, Y=y) / P(X=x)
 cpd = nltk.ConditionalProbDist(cfd, nltk.MLEProbDist)
-cpd_yx_table = np.zeros((27, 27))
 
+cpd_yx_table = np.zeros((27, 27))
 for x in range(0, 27):
     for y in range(0, 27):
         # print("(" + conditions[x] + ", " + conditions[y] + ") :" + str(cpd[conditions[x]].prob(conditions[y])))
         cpd_yx_table[x][y] = cpd[conditions[x]].prob(conditions[y])
 
+# Dataframe of probability distribution P(Y=y|X=x) for ordered pair XY
 cpd_yx_df = pd.DataFrame(cpd_yx_table, index=conditions, columns=conditions)
-print("* P(y|x) table")
-print(cpd_yx_df)
-cpd_yx_df.to_csv(r'C:\Users\Leki\Desktop\Dooo\GCT561_ScientificThinking\HW1\df_cpd_yx.csv')
 
-# Show plot
+print("* P(y|x) table")
+print(cpd_yx_df)       # print the probability distribution
+
+# Save the conditional probability distribution data
+cpd_yx_df.to_csv(r'C:\Users\Leki\Desktop\Dooo\GCT561_ScientificThinking\HW1\result\df_cpd_yx.csv')
+
+# Draw and save the plot for the probability distribution
 drawPlot(cpd_yx_table, conditions, conditions,
          "Conditional probability distribution of P(Y=y|X=x) for ordered pair XY", 50,
          "fig_cpd_yx")
 
+
+print()
 
 # 3-2. P(x|y)
 # For ordered pair XY, compute P(X=x|Y=y) = P(X=x, Y=y) / P(Y=y)
@@ -174,24 +190,16 @@ for x in range(0, 27):
         else:               # alphabet
             cpd_xy_table[x][y] = cfd[conditions[x]][conditions[y]] / alice_raw.count(alphabet[y])
 
+# Dataframe of probability distribution P(X=x|Y=y) for ordered pair XY
 cpd_xy_df = pd.DataFrame(cpd_xy_table, index=conditions, columns=conditions)
+
 print("* P(x|y) table")
-print(cpd_xy_df)
-cpd_xy_df.to_csv(r'C:\Users\Leki\Desktop\Dooo\GCT561_ScientificThinking\HW1\df_cpd_xy.csv')
+print(cpd_xy_df)       # print the probability distribution
 
-# Show plot
+# Save the conditional probability distribution data
+cpd_xy_df.to_csv(r'C:\Users\Leki\Desktop\Dooo\GCT561_ScientificThinking\HW1\result\df_cpd_xy.csv')
+
+# Draw and save the plot for the probability distribution
 drawPlot(cpd_xy_table, conditions, conditions,
-         "Conditional probability distribution of P(X=x|Y=y) for ordered pair XY", 50,
+         "Conditional probability distribution P(X=x|Y=y) for ordered pair XY", 50,
          "fig_cpd_xy")
-
-''' To Do '''
-# 1. Probability distribution over the 27 outcomes for a randomly selected letter
-# fd = nltk.probability.FreqDist(alice_raw)
-# print(fd.N())   # Count all letters in alice data
-# print(fd['a'])  # Count 'a' letters in alice data
-
-# 2. Probability distribution over the 27x27 possible bigrams xy
-
-# 3. Conditional probability
-#  3-1. P(y|x)
-#  3-2. P(x|y)
---
+++ HW1/readme.txt
@@ -0,0 +1,25 @@
+¡Ø To run the code, you need to download python packages:
+- numpy
+- nltk
+- matplotlib
+- pandas
+
+¡Ø If you first run the code, you need to download the data of gutenberg corpus
+- uncomment the 51 line in the code
+
+¡Ø When run the code, all results of probability distributions are saved in the result folder.
+1. Probability distribution over the 27 outcomes for a randomly selected letter
+- data: df_letters.csv
+- figure: fig_letters.png
+
+2. Probability distribution over the 27x27 possible bigrams for ordered pair xy
+- data: df_bigram.csv
+- figure: df_bigram.png
+
+3. Conditional probability distribution over P(y|x)
+- data: df_cpd_yx.csv
+- figure: fig_cpd_yx.png
+
+4. Conditional probability distribution over P(x|y)
+- data: df_cpd_xy.csv
+- figure: fig_cpd_xy.png
